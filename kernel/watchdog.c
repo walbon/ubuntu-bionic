b68ce5a977c3c34ea3abc3f48e96c7cc8e855711
Stephen Rothwell <sfr@Blade23.ozlabs.ibm.com>
2013-12-12 15:51:21 +1100

    nowatchdog-on-virt.patch
    Disable watchdog on virtual machines.
    
    For various reasons, VMs seem to trigger the soft lockup detector a lot,
    in cases where it's just not possible for a lockup to occur.
    (Example: https://bugzilla.redhat.com/show_bug.cgi?id=971139)
    
    In some cases it seems that the host just never scheduled the app running
    the VM for a very long time (Could be the host was under heavy load).
    
    Just disable the detector on VMs.
    
    Signed-off-by: Dave Jones <davej@redhat.com>

--- kernel/watchdog.c
+++ kernel/watchdog.c
@@ -24,6 +24,7 @@
 #include <linux/sysctl.h>
 #include <linux/smpboot.h>
 #include <linux/sched/rt.h>
+#include <linux/dmi.h>
 
 #include <asm/irq_regs.h>
 #include <linux/kvm_para.h>
@@ -95,6 +96,32 @@ static int __init nosoftlockup_setup(char *str)
 }
 __setup("nosoftlockup", nosoftlockup_setup);
 /*  */
+
+static int disable_watchdog(const struct dmi_system_id *d)
+{
+	printk(KERN_INFO "watchdog: disabled (inside virtual machine)\n");
+	watchdog_enabled = 0;
+	return 0;
+}
+
+static const struct dmi_system_id watchdog_virt_dmi_table[] = {
+	{
+		.callback = disable_watchdog,
+		.ident = "VMware",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "VMware, Inc."),
+		},
+	},
+	{
+		.callback = disable_watchdog,
+		.ident = "Bochs",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "Bochs Bochs"),
+		},
+	},
+	{}
+};
+
 
 /*
  * Hard-lockup warnings should be triggered after just a few seconds. Soft-
@@ -543,6 +570,8 @@ static struct smp_hotplug_thread watchdog_threads = {
 
 void __init lockup_detector_init(void)
 {
+	dmi_check_system(watchdog_virt_dmi_table);
+
 	set_sample_period();
 	if (smpboot_register_percpu_thread(&watchdog_threads)) {
 		pr_err("Failed to create watchdog threads, disabled\n");
